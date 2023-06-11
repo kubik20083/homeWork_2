8f4288bd7b52c479ed976ed0d5353d79ae1f8e2d
kubik20083 <kubik20083@mail.ru>
2023-06-11 15:54:06 +0300

    inin repo

---
+++ task1.java
@@ -0,0 +1,6 @@
+// 1) Дана строка sql-запроса "select * from students WHERE ". 
+// Сформируйте часть WHERE этого запроса, используя StringBuilder.
+// Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Пример данной строки {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// Вывод: select * from students WHERE name=Ivanov AND country=Russia AND city=Moscow
